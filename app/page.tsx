dcd32700951a970b88bcc08583d42588b74eed1e
Pete Petrash <pete.petrash@gmail.com>
2025-11-11 09:41:56 -0800

    Refactor HomePage layout to enhance responsiveness; adjust main and div structures for improved alignment and flexibility across different screen sizes.

--- app/page.tsx
+++ app/page.tsx
@@ -27,12 +27,12 @@ export default function HomePage({
       }
     >
       <ContentLayout noScroll>
-        <main className="relative grid h-full max-h-[800px] min-h-0 w-full max-w-[2000px] grid-cols-1 gap-10 overflow-hidden md:grid-cols-[minmax(0,1fr)_minmax(0,1fr)] md:p-6">
-          <div className="relative z-10 flex max-w-[600px] min-w-[500px] flex-col justify-end overflow-y-auto pb-[8vh] pl-6">
+        <main className="relative h-full max-h-[800px] min-h-0 w-full max-w-[2000px] overflow-hidden flex flex-col gap-10 md:p-6 lg:flex-row">
+          <div className="relative z-10 flex w-full flex-col justify-end overflow-y-auto pb-[8vh] pl-6 lg:grow-0 lg:shrink lg:basis-[33vw] lg:min-w-[320px] lg:max-w-[33vw]">
             <HomeHero />
           </div>
 
-          <div className="relative hidden h-full min-h-0 w-full min-w-0 items-center lg:flex">
+          <div className="relative hidden h-full min-h-0 w-full min-w-0 items-center lg:flex lg:flex-1">
             <div className="h-[600px] w-full">
               <ChatShowcase />
             </div>
