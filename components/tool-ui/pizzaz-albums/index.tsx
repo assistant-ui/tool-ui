a44778cf436a98d61aca0b1e2cf233f84eeb005c
Pete Petrash <pete.petrash@gmail.com>
2026-01-19 13:04:11 -0800

    remove dead code

--- components/tool-ui/pizzaz-albums/index.tsx
+++
@@ -1,8 +0,0 @@
-export { PizzazAlbums } from "./pizzaz-albums";
-export { DEFAULT_ALBUMS } from "./data";
-export type {
-  Album,
-  AlbumPhoto,
-  DisplayMode,
-  PizzazAlbumsProps,
-} from "./types";
